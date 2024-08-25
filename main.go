605b1f6d353633cc28d99c6bb60ce6c1a5341e2a
Marlon Peseke <mpeseke@gmail.com>
2024-08-25 11:45:59 -0600

    feat(dice rolls): dice rolls are now based on min face value, max face value, and the amount of dice being rolled

--- main.go
+++ main.go
@@ -10,13 +10,25 @@ import (
 
 func main() {
 	fmt.Println("Hello, Go!")
-	fmt.Println(roll_dice(1, 20)) //simulate rolling d20
-	fmt.Println(roll_dice(1, 6))  //simulate rolling d6
 	choose_race()
+
+	fmt.Println(roll_dice(1, 6, 3))
 }
 
-func roll_dice(min, max int) int {
-	return rand.Intn((max+1)-min) + min
+func roll_dice(min, max, no_of_die int) ([]int, int) {
+	var rolls []int
+
+	for i := 0; i < no_of_die; i++ {
+		rolls = append(rolls, rand.Intn((max+1)-min)+min)
+	}
+
+	total := 0
+
+	for _, rolls := range rolls {
+		total += rolls
+	}
+
+	return rolls, total
 }
 
 func choose_race() string {
@@ -40,13 +52,13 @@ func choose_race() string {
 		if input == "1" || input == "2" {
 			if input == "1" {
 				fmt.Println("You have chosen to fight as the noble human, raise your fist and fight for death and glory in Valholl.")
+				return "Human"
 			}
 
 			if input == "2" {
 				fmt.Println("You have chosen to fight as the mighty Immortal, cursed to wander Urth for all eternity, swept to and fro by the whims of the gods.")
+				return "Immortal"
 			}
-
-			return input
 		}
 
 		fmt.Println("Invalid choice. Please choose again. ")
